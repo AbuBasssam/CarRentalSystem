8239f4dc4a8da24c4cee03393fa60e5dc24ecada
Abdul Rahman <programmerabubassam@gmail.com>
2025-12-31 22:53:49 +0300

    Add ResendVerificationCodeCommand class

---
+++ Application/Features/AuthFeature/Commands/ResendVerification/ResendVerificationCodeCommand.cs
@@ -0,0 +1,5 @@
+﻿using Application.Models;
+using MediatR;
+
+namespace Application.Features.AuthFeature;
+public record ResendVerificationCodeCommand(string VerificationToken) : IRequest<Response<string>>;
