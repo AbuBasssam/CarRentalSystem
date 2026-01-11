e48539ceb75d8ee212182229625067361b38d515
Abdul Rahman <programmerabubassam@gmail.com>
2026-01-11 15:15:07 +0300

    Modify IRequestContext

--- Application/Interfaces/IRequestContext.cs
+++ Application/Interfaces/IRequestContext.cs
@@ -4,9 +4,9 @@ public interface IRequestContext : IScopedService
 {
     string? AuthToken { get; }
     string? ClientIP { get; }
-    string Language { get; }
-    int UserId { get; }
-    string TokenJti { get; }
-    string Email { get; }
+    string? Language { get; }
+    int? UserId { get; }
+    string? TokenJti { get; }
+    string? Email { get; }
 
 }
